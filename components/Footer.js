cbe8952a4204cc1a3cbb8e3fb7b4a6fc88c7b98c
Kari Laari <kari@karin-atk.fi>
2022-02-14 12:26:08 +0200

    Correct social link

--- components/Footer.js
+++ components/Footer.js
@@ -4,19 +4,19 @@ export default function Nav() {
     return (
         <footer>
             <div className="links">
-                <a href="https://www.soundcloud.com">
+                <a href="https://soundcloud.com/tapani-rinne">
                     <img alt="soundcloud" src="./assets/icons/soundcloud.svg" />
                 </a>
-                <a href="https://www.spotify.com">
+                <a href="https://open.spotify.com/artist/3444FAqjOpZGe7qdyshHg1">
                     <img alt="spotify" src="./assets/icons/spotify.svg" />
                 </a>
-                <a href="https://www.bandcamp.com">
+                <a href="https://tapanirinne.bandcamp.com/">
                     <img alt="bandcamp" src="./assets/icons/bandcamp.svg" />
                 </a>
-                <a href="https://www.instagram.com">
+                <a href="https://www.instagram.com/tapanirinne/">
                     <img alt="instagram" src="./assets/icons/instagram.svg" />
                 </a>
-                <a href="https://www.facebook.com">
+                <a href="https://www.facebook.com/tapanirinnemusic">
                     <img alt="facebook" src="./assets/icons/facebook.svg" />
                 </a>
             </div>
